f439fa0072e82d9b767c19657c6f3efe5ab10437
avadhut <avadhutvagare321@gmail.com>
2020-05-09 20:49:25 +0530

    Contact page design

--- contact.php
+++ contact.php
@@ -1,4 +1,4 @@
-<? include 'header.php'?>
+<?php include('header.php'); ?>
 	<!-- banner -->
 	<div class="banner-1">
 	</div> 
@@ -12,10 +12,10 @@
 			</div>				
 				<div class="contact-form">
 					<form action="#" method="post">
-						<input type="text" name="Name" placeholder="Name" required="">
+						<input type="text" name="name" placeholder="Name" required="">
 						<input type="email" class="email" name="Email" placeholder="Email" required="">
-						<input type="text" name="Phone" placeholder="Phone" required="">
-						<textarea name="Message" placeholder="Message" required=""></textarea>
+						<input type="text" name="phone" placeholder="Mobile No" required="">
+						<textarea name="comment" placeholder="Comment" required=""></textarea>
 						<input type="submit" value="SUBMIT" >
 					</form>
 				</div>
@@ -26,17 +26,78 @@
 	<!-- map -->
 	<div class="map w3layouts">
 		<div class="col-md-8 map-left">
-			<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d57537.641430789925!2d-74.03215321337959!3d40.719122105634035!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c24fa5d33f083b%3A0xc80b8f06e177fe62!2sNew+York%2C+NY%2C+USA!5e0!3m2!1sen!2sin!4v1456152197129" allowfullscreen=""></iframe>
+			<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15111.231740528474!2d73.85701187899802!3d18.762114196298935!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc2c9f585ac70d1%3A0x7269f7854109659!2sChakan%2C+Maharashtra+410501!5e0!3m2!1sen!2sin!4v1496219622595" height="100%" allowfullscreen></iframe>
 		</div>
 		<div class="col-md-4 map-wthree-right"> 
 			<h4><span class="glyphicon glyphicon-home" aria-hidden="true"></span> Address</h4>
-			<p>CG 123 Sebas, Ba New York, USA</p>
-			<h4><span class="glyphicon glyphicon-phone" aria-hidden="true"></span>Phone</h4>
-			<p> +00 111 2222 333 </p>
+			<p>Wadgaon Ghenand Rd, Bhose, Chakan, Tal - Khed, Dist - Pune Pin Code: 410501</p>
+			<h4><span class="glyphicon glyphicon-phone" aria-hidden="true"></span>Mobile No</h4>
+			<p> +91 968 9956 137,  +91 968 9956 137</p>
 			<h4><span class="glyphicon glyphicon-envelope" aria-hidden="true"></span>Email</h4>
-			<p class="agile-last"><a href="mailto:info@example.com">mail@example.com</a></p> 
+			<p class="agile-last"><a href="mailto:info@example.com">holyangle.ems@gmail.com</a></p> 
 		</div>
 		<div class="clearfix"> </div>
+		<div class="service-content">
+		    <div class="row">
+		    	<div class="container" style="padding-top: 20px; padding-top: 40px;
+    padding-bottom: 40px;">
+		    		<!-- Start single service -->
+				        <div class="col-md-3 col-sm-3">
+					        <div class="single-service wow zoomIn" style="visibility: visible; animation-name: zoomIn;">
+					                  <h4 class="service-title"><a href="C.php">Play Group -Sr.KG </a></h4>
+					                  <p>Balaji Nagar, Medankarwadi,
+					                      Chakan, Tal. Khed,
+					                      Pune. 410501
+					                      Mob. 9657015584</p>
+					        </div>
+				        </div>
+				       <!-- End single service -->
+				       <!-- Start single service -->
+				        <div class="col-md-3 col-sm-3">
+				            <div class="single-service wow zoomIn" style="visibility: visible; animation-name: zoomIn;">
+				               <h4 class="service-title"><a href="transportation.php.php">Play Group -1 Std</a></h4>
+				              <p>Opp, Kshiti Vihar,
+				                  Talegaon Shikrapur,
+				                  Tal. Shirur, Pune. 412208
+				                  Mob. 8308614333
+				              </p>
+				            </div>
+				        </div>
+				       <!-- End single service -->
+				       <!-- Start single service -->
+				      <div class="col-md-3 col-sm-3">
+				          <div class="single-service wow zoomIn" style="visibility: visible; animation-name: zoomIn;">
+				              <h4 class="service-title"><a href="lab_computer.php.php">Play Group -Sr.KG</a></h4>
+				              <p>A/P Koyali, Ranubai Road
+				                  Tal. Khed, Pune -410501
+				                  Mob. 9075757810
+				              </p>
+				          </div>
+				      </div>
+				      <!-- End single service -->
+				      <!-- Start single service -->
+				      <div class="col-md-63 col-sm-3">
+				          <div class="single-service wow zoomIn" style="visibility: visible; animation-name: zoomIn;">
+				              <h4 class="service-title">
+				                <a href="lab_facilities.php.php">
+				                    Play Group -Sr.KG
+				                </a>
+				              </h4>
+				              <p>A/P Zhitraimala Near Bhama Perolium Ambethan Road,
+				                  Chakan, Tal Khed, Pune 410501
+				                  Mob. 7218072380
+				              </p>
+				          </div>
+				      </div>
+				      <!-- End single service -->
+
+
+
+
+		    	</div>	  
+			</div>
+		</div>
+
 	</div>
 	<!-- //map -->
-	<? include 'footer.php'?>
+	<?php include('footer.php');?>
